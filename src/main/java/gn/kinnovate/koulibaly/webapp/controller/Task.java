93f9a002a40f7237b48a91707f9059326ae412c5
koulibaly <koulibalyamadou@outlook.com>
2024-07-08 14:26:14 +0000

    commit

---
+++ src/main/java/gn/kinnovate/koulibaly/webapp/controller/Task.java
@@ -0,0 +1,20 @@
+package gn.kinnovate.koulibaly.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Task {
+
+    @RequestMapping("/tasks")
+    public String index(Model model){
+        model.addAttribute("task", new Task());
+        return "tasks/index";
+    }
+
+    public String save(){
+
+        return 
+    }
+}
---
+++ src/test/java/gn/kinnovate/koulibaly/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package gn.kinnovate.koulibaly.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
